46d09a047065ef1e237ba7283bd28b28d09524c0
NIKOLAY PETROV NIKOLOV <Nikolay.Petrov@alu.uclm.es>
2024-04-24 01:02:41 +0200

    - Se ha corregido un comentario

--- field.class.php
+++ field.class.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Privacy Subsystem implementation for datafield_text.
+ * Implementation for the new datafield.
  *
  * @package    datafield
  * @copyright  2018 Carlos Escobedo <carlos@moodle.com>
